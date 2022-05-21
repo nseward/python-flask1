5c06d248635154a5a6830fedaf47f41bd63d881e
nseward <neil.seward@gmail.com>
2022-05-21 15:21:29 +0100

    allow app to run via flask (not flask)

--- app.py
+++ app.py
@@ -6,3 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+if __name__ == '__main__':
+    create_app().run(host='0.0.0.0', port=5000, debug=True)
