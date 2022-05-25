0295d8017ca46183e531b1edc72cc486760db64b
Neil <neil.seward@gmail.com>
2022-05-25 11:42:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World! version 1.3</p>"
+    return "<p>Hello, World! version 1.4</p>"
 
 
 if __name__ == '__main__':
     
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=3000, debug=True)
