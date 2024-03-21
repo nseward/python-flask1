ea90639343f958de9560975fc4d6f5b54047df71
Neil <11341303+nseward@users.noreply.github.com>
2024-03-21 00:16:11 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World! version 1.6</p>"
+    return "<p>Hello, World! version 1.7</p>"
 
 
 if __name__ == '__main__':
